df4ed7f860d6921496ac4d43c818458413da683b
Luis Francisco Marroquin Ortiz <lfortiz.agt@esieduc.org>
2024-07-16 14:09:22 -0600

    Seguridad con Spring Boot usando JWT

---
+++ src/main/java/org/ada/school/utils/Constants.java
@@ -0,0 +1,41 @@
+package org.ada.school.utils;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+
+import java.util.Date;
+import java.util.Map;
+
+public class Constants {
+    public static final String CLAIMS_ROLES_KEY = "roles";
+    public static final String COOKIE_NAME = "JWT_TOKEN";
+    private static final String SECRET_KEY = "Spiderman";
+    public static final int TOKEN_DURATION_MINUTES = 60;
+
+    public static String generateToken(String username, Map<String, Object> claims) {
+        return Jwts.builder()
+                .setClaims(claims)
+                .setSubject(username)
+                .setIssuedAt(new Date(System.currentTimeMillis()))
+                .setExpiration(new Date(System.currentTimeMillis() + 1000 * 60 * TOKEN_DURATION_MINUTES)) // Usa la constante
+                .signWith(SignatureAlgorithm.HS256, SECRET_KEY)
+                .compact();
+    }
+
+    public static Claims extractClaims(String token) {
+        return Jwts.parser()
+                .setSigningKey(SECRET_KEY)
+                .parseClaimsJws(token)
+                .getBody();
+    }
+
+    public static boolean isTokenValid(String token, String username) {
+        final String extractedUsername = extractClaims(token).getSubject();
+        return (extractedUsername.equals(username) && !isTokenExpired(token));
+    }
+
+    private static boolean isTokenExpired(String token) {
+        return extractClaims(token).getExpiration().before(new Date());
+    }
+}
